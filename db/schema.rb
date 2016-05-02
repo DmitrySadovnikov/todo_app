47587e1bf5c87266252ee4802b6e8a60157bb3d7
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-05-02 08:25:35 +0300

    replece sqlite to pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160501173825) do
+ActiveRecord::Schema.define(version: 20160502044736) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "todos", force: :cascade do |t|
     t.string   "title"
